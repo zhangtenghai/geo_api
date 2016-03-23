e8a43be6893b375a0f8a0b36264714f572f1276a
bruce <zth5906@gmail.com>
2016-03-23 16:55:58 +0800

    merge

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -9,4 +9,47 @@ describe "test location" do
     p manager.proxy.class.name
     expect(manager.proxy.class.name).to eq("GeoApi::Qq")
   end
+
+  it "test api" do
+    qq_map = GeoApi.setup do |config|
+      config.server = 'http://apis.map.qq.com/ws/geocoder/v1/'
+      config.key = 'NM2BZ-V3HRG-ZHMQO-IPJQD-Q375T-EPFRZ'
+      config.vendor = 'QQ'
+    end
+
+    result = qq_map.get_coordinate_from_string("上海市上海市黄浦区思南路115弄4号")
+    p result
+    expect(result[:longitude]).not_to be_empty
+    expect(result[:latitude]).not_to be_empty
+
+    result = qq_map.get_coordinate_from_string("思南路115弄", "上海市")
+    expect(result[:longitude]).not_to be_empty
+    expect(result[:latitude]).not_to be_empty
+
+    result = qq_map.get_coordinate_from_string("")
+    
+    expect(result).to eq(nil)
+
+
+    baidu = GeoApi.setup do |config|
+        config.server = "http://api.map.baidu.com/geocoder/v2/"
+        config.key = "m48tmnl9BPwnhNOctmrVKMRa"
+        config.vendor = "BAIDU"
+    end
+
+    result = baidu.get_coordinate_from_string("上海市上海市黄浦区思南路115弄4号")
+    p result
+    expect(result[:longitude]).not_to be_empty
+    expect(result[:latitude]).not_to be_empty
+
+    result = baidu.get_coordinate_from_string("思南路115弄", "上海市")
+   
+    expect(result[:longitude]).not_to be_empty
+    expect(result[:latitude]).not_to be_empty
+
+    result = baidu.get_coordinate_from_string("")
+    
+    expect(result).to eq(nil)
+
+  end
 end
