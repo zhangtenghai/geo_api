782aceca64b5e6e2999f5aa458d5e541a2236c94
bruce <zth5906@gmail.com>
2016-03-23 16:42:00 +0800

    merge

--- lib/geo_api.rb
+++ lib/geo_api.rb
@@ -3,79 +3,18 @@ require 'geo_api/configuration'
 require "geo_api/baidu"
 require "geo_api/gaode"
 require "geo_api/qq"
+require "geo_api/manager"
 require 'logger'
 
 module GeoApi
-    class << self
-      def setup
-        GeoApi.new(yield config)
-      end
-
-      def config
-        @config ||= Configuration.new
-      end
+  class << self
+    def setup
+      yield config
+      Manager.new(config)
     end
 
-    def initialize(config)
-      self.config = config
+    def config
+      @config ||= Configuration.new
     end
-
-    def config(value)
-      @config = value
-    end
-
-    def logger
-      @logger ||= Logger.new(STDOUT)
-    end
-
-    def proxy
-      @proxy ||= get_proxy
-    end
-
-    def get_location_from_string(location)
-      unless location.nil? || location.length == 0
-        formated_address = location.split(/,|-|;|>|:|\+|\^/)
-        databack = Hash.new
-        databack["province"] = formated_address[0] if formated_address.length > 0
-        databack["city"] = formated_address[1] if formated_address.length > 1
-        databack["region"] = formated_address[2] if formated_address.length > 2
-        databack["detail"] = formated_address[3] if formated_address.length > 3
-        databack["latitude"] = ""
-        databack["longitude"] = ""
-
-        if ["重庆市", "上海市", "北京市", "天津市"].include?(databack["province"])
-          databack["region"] = databack["city"]
-          databack["city"] = databack["province"]
-        end
-
-        return databack
-      else
-        return nil
-      end
-    end
-
-    def get_location_from_coordinate(longitude, latitude)
-      proxy.get_location_from_coordinate(longitude, latitude)
-    end
-
-    def get_coordinate_from_string(location, city = nil)
-      proxy.get_coordinate_from_string(location, city = nil)
-    end
-
-    def formatted_address(address)
-      proxy.formatted_address(address)
-    end
-
-    def get_proxy
-      case config.vendor
-      when 'BAIDU'
-        return GeoApi::Baidu.new(config)
-      when 'GAODE'
-        return GeoApi::Gaode.new(config)
-      when 'QQ'
-        return GeoApi::Qq.new(config)
-      else
-        raise '不支持的Vendor'
-      end
-    end
+  end
 end
